7f7d17fd97000975254a36c4d7169284e13bb796
bwheeler98 <100613516+bwheeler98@users.noreply.github.com>
2022-05-20 17:21:14 -0400

    Add files via upload

---
+++ Mission-to-Mars/app.py
@@ -0,0 +1,27 @@
+## import dependendencies
+from flask import Flask, render_template, redirect, url_for
+from flask_pymongo import PyMongo
+import scraping
+
+## set up Flask
+app = Flask(__name__)
+
+## use flask_pymongo to set yp mongo connection
+app.config['MONGO_URI'] = "mongodb://localhost:27017/mars_app"
+
+## define html route
+@app.route("/")
+def index():
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars)
+
+@app.route("/scrape")
+def scrape():
+    mars = mongo.db.mars
+    mars_data = scraping.scrape_all()
+    mars.update_one({}, {"$set":mars_data}, upsert=True)
+    return redirect('/', code=302)
+
+## run the Flask
+if __name__ == "__main__":
+    app.run()
