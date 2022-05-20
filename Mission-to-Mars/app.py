0f6b45661a16542ccb6d495284d4fe67e52a6946
bwheeler98 <100613516+bwheeler98@users.noreply.github.com>
2022-05-20 19:41:09 -0400

    Add files via upload

--- Mission-to-Mars/app.py
+++ Mission-to-Mars/app.py
@@ -3,10 +3,12 @@ from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 import scraping
 
+
+
 ## set up Flask
 app = Flask(__name__)
-
-## use flask_pymongo to set yp mongo connection
+mongo = PyMongo(app)
+## use flask_pymongo to set pymongo connection
 app.config['MONGO_URI'] = "mongodb://localhost:27017/mars_app"
 
 ## define html route
